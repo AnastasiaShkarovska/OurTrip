d07df300980e0946d2d291fb73290c62a27543ca
anastasiaskarovska <a.shkarovska@ukma.edu.ua>
2026-06-15 15:40:20 +0300

    Trip page

---
+++ trip.js
@@ -0,0 +1,9 @@
+function show(id){
+    document.getElementById('plany').style.display = 'none';
+    document.getElementById('trips').style.display = 'none';
+    document.getElementById(id).style.display = 'block';
+}
+ function details(id){
+   document.getElementById('first').style.display='none'
+   document.getElementById(id).style.display = 'block';
+ }
