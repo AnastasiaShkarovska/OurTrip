fd593515c08ae211d225300ef43576c941001a80
anastasiaskarovska <a.shkarovska@ukma.edu.ua>
2026-07-04 20:19:03 +0300

    Initial commit

--- trip.js
+++ trip.js
@@ -3,7 +3,14 @@ function show(id){
     document.getElementById('trips').style.display = 'none';
     document.getElementById(id).style.display = 'block';
 }
- function details(id){
-   document.getElementById('first').style.display='none'
-   document.getElementById(id).style.display = 'block';
- }
+window.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("click", (e) => {
+    document.querySelectorAll("details[open]").forEach(d => {
+      const card = d.closest(".one-choice");   // картка з відкритими деталями
+      // якщо клікнули ПОЗА цією карткою — закрити
+      if (!card.contains(e.target)) {
+        d.open = false;
+      }
+    });
+  });
+});
--- trip/trip.js
+++
@@ -1,6 +0,0 @@
- function show(id) {
-            document.getElementById('plany').style.display = 'none';
-            document.getElementById('trips').style.display = 'none';
-            document.getElementById(id).style.display = 'block';
- 
- }
